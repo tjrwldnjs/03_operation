8c231b231f85bad86ad741e18dbc6a371bbf17aa
tjrwldnjs <sue06024@naver.com>
2018-10-02 16:29:22 +0900

    03_operation s,m,h

--- Project1.c
+++ Project1.c
@@ -5,15 +5,13 @@
 
 int main(int argc, char *argv[]) {
 	
-	int year;
-	int result;
+	int sec;
 	
 	printf("input : ");
-	scanf("%d", &year);
+	scanf("%d", &sec);
 	
-	result = (year%4 == 0 && year%100 !=0) || (year%400 == 0);
 	
-	printf("leap year? %d\n", result);
+	printf("sec %i is %i : %i : %i\n",sec, sec/3600, (sec%3600)/60, sec%60);
 	
 	
 	return 0;
